3e61e91b2ed128e95ae4924d5d6a6f076089526f
Spedry <spedrysk@gmail.com>
2023-12-17 20:07:23 +0100

    freezing problem solved

--- src/app/components/pages/dashboard/dashboard.component.ts
+++ src/app/components/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import {Component, inject, Input, OnDestroy, OnInit} from "@angular/core";
 import {ConfigService} from "../../../services/config.service";
 import {NGXLogger} from "ngx-logger";
 import {map} from "rxjs/operators";
-import {Breakpoints, BreakpointObserver} from '@angular/cdk/layout';
+import {Breakpoints, BreakpointObserver, BreakpointState} from '@angular/cdk/layout';
 import {WidgetComponent} from "../../widgets/widget/widget.component";
 import {MatGridListModule} from "@angular/material/grid-list";
 import {AsyncPipe, NgSwitchDefault, NgSwitch, NgForOf, NgSwitchCase} from "@angular/common";
@@ -11,6 +11,7 @@ import {Subscription} from "rxjs";
 import {PageContent} from "../../../models/pageContent";
 import {PageConfigService} from "../../../services/page-config.service";
 import {AllWidget, PageWidgets} from "../../../models/pageWidgets";
+import {CardLayout} from "../../../models/cardLayout";
 
 
 @Component({
@@ -32,36 +33,14 @@ import {AllWidget, PageWidgets} from "../../../models/pageWidgets";
 export class DashboardComponent implements OnInit, OnDestroy {
   private subscription?: Subscription;
   pageWidgets?: PageWidgets;
-  private breakpointObserver = inject(BreakpointObserver);
   /*miniWidgets : AllWidget = {widgetType: "miniWidgets", widgets: []}
   mediumWidgets : AllWidget = {widgetType: "mediumWidgets", widgets: []}
   largeWidgets : AllWidget = {widgetType: "largeWidgets", widgets: []}*/
 
-
-  /** Based on the screen size, switch from standard to one column per row */
-  cardLayout = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({matches}) => {
-      if (matches) {
-        return {
-          columns: 1,
-          miniCard: {cols: 1, rows: 1},
-          mediumCard: {cols: 1, rows: 2},
-          largeCard: {cols: 1, rows: 4}
-        };
-      }
-
-      return {
-        columns: 6, // GRID COLUMNS
-        miniCard: {cols: 1, rows: 1},
-        mediumCard: {cols: 2, rows: 3},
-        largeCard: {cols: 2, rows: 3}
-      };
-    })
-  );
-
   constructor(private config: ConfigService,
               private log: NGXLogger,
-              private pageConfig: PageConfigService) {
+              private pageConfig: PageConfigService,
+              private breakpointObserver: BreakpointObserver) {
     /*config.getWidgets("miniWidgets").subscribe((widgets: Widgets) => {
       this.log.debug("Retrieved", widgets)
       if (widgets)
@@ -87,15 +66,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
     return {widgetType: widgetType, widgets: []};
   }
 
-  getContext<T>(context: string): Subscription {
-    return this.config.getConfig(context).subscribe((obj) => {
-      this.log.debug("Retrieved", obj)
-      if (obj)
-        return <T>obj;
-      return {}
-    })
-  }
-
   ngOnInit(): void {
     this.subscription = this.pageConfig.selectedValue$.subscribe((value: PageContent) => {
       this.config.getConfig<PageWidgets[]>("pageWidgets").subscribe((pageWidgets: PageWidgets[]) => {
@@ -106,7 +76,46 @@ export class DashboardComponent implements OnInit, OnDestroy {
           this.pageWidgets = pageWidgets.find(item => item.id == pageContent.id);
       })
     });
+     this.breakpointObserver.observe(Breakpoints.Handset).subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          this.cardLayout = {
+            columns: 1,
+            miniCard: {cols: 1, rows: 1},
+            mediumCard: {cols: 1, rows: 2},
+            largeCard: {cols: 1, rows: 4}
+          };
+        } else {
+          this.cardLayout = {
+            columns: 6, // GRID COLUMNS
+            miniCard: {cols: 1, rows: 1},
+            mediumCard: {cols: 2, rows: 3},
+            largeCard: {cols: 2, rows: 3}
+          };
+        }
+      });
   }
+  cardLayout?: CardLayout;
+  /** Based on the screen size, switch from standard to one column per row */
+  /*cardLayout = this.breakpointObserver.observe(Breakpoints.Web).pipe(
+    map(({matches}) => {
+      if (matches) {
+        return {
+          columns: 1,
+          miniCard: {cols: 1, rows: 1},
+          mediumCard: {cols: 1, rows: 2},
+          largeCard: {cols: 1, rows: 4}
+        };
+      }
+
+      return {
+        columns: 6, // GRID COLUMNS
+        miniCard: {cols: 1, rows: 1},
+        mediumCard: {cols: 2, rows: 3},
+        largeCard: {cols: 2, rows: 3}
+      };
+    })
+  );*/
+
   ngOnDestroy(): void {
     if (this.subscription)
       this.subscription.unsubscribe();
---
+++ src/app/models/cardLayout.ts
@@ -0,0 +1,11 @@
+export interface CardLayout {
+  columns:    number;
+  miniCard:   Card;
+  mediumCard: Card;
+  largeCard:  Card;
+}
+
+export interface Card {
+  cols: number;
+  rows: number;
+}
